ee86ed0cb133f5f3897addfae8290d28a5ef86a3
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2022-09-17 17:25:26 +0300

    lexer analysis

---
+++ src/Token.ts
@@ -0,0 +1,14 @@
+import TokenType from "./TokenType";
+
+export default class Token {
+    type: TokenType;
+    text: string;
+    pos: number;
+
+    constructor(type: TokenType, text: string, pos: number) {
+        this.type = type;
+        this.text = text;
+        this.pos = pos;
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const str: string = "ТЕСТ";
+import Lexer from "./Lexer";
 
-console.log(str);
+const code =
+    `
+    сумма РАВНО 5 МИНУС 9;
+    суммадва РАВНО 0 МИНУС 6;
+    КОНСОЛЬ сумма;
+    КОНСОЛЬ суммадва;
+    КОНСОЛЬ сумма МИНУС суммадва ПЛЮС ( 5 ПЛЮС 3 );
+    `
+
+const lexer = new Lexer(code);
+lexer.lexAnalysis();
+// console.log(lexer.tokenList)
